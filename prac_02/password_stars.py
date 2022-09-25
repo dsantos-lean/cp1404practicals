fa0b17f8500ca4a6b457ba5fd4b9767638c91403
dsantos-lean <lean.delossantos@my.jcu.edu.au>
2022-09-25 22:29:31 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,21 @@
 PASSWORD_LENGTH = 10
-password = input("Enter password: ")
-while len(password) != PASSWORD_LENGTH:
-    print("Invalid length")
+
+
+def main():
+    password = get_password()
+    print_stars(password)
+
+
+def print_stars(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Enter password: ")
-print("*" * len(password))
+    while len(password) != PASSWORD_LENGTH:
+        print("Invalid length")
+        password = input("Enter password: ")
+    return password
+
+
+main()
