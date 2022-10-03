8f54d6fece5e0fb4cc86d2a7c96b6cea948cb339
dsantos-lean <lean.delossantos@my.jcu.edu.au>
2022-10-03 14:11:50 +1000

    Add string formatting, randoms, capitalist conrad programs

---
+++ prac_03/randoms.py
@@ -0,0 +1,7 @@
+import random
+
+# print(random.randint(5, 20))  # line 1 = Smallest is 5, largest is 20
+# print(random.randrange(3, 10, 2))  # line 2 = Smallest is 3, cannot produce 4
+# print(random.uniform(2.5, 5.5))  # line 3 = Smallest 2.5, largest 5.5
+
+print(random.randint(1, 100))
