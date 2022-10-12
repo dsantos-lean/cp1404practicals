7f87096bae2953f66f47e425ea53bb2d0053de6d
dsantos-lean <lean.delossantos@my.jcu.edu.au>
2022-10-13 09:57:59 +1000

    Add lists warmup, total income, subject reader programs

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,14 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# 3
+# 2
+# 1
+# 314159
+# 1
+# True
+# False
+# False
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
