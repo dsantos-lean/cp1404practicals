5268bc8f17123e873abde8641e1b02dd2c62a9b0
dsantos-lean <lean.delossantos@my.jcu.edu.au>
2022-10-19 15:24:04 +1000

    Update hex colours program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,6 +1,4 @@
 NAME_TO_CODE = {"Amber": "#ffbf00", "AntiqueWhite": "#faebd7", "Aqua": "#00ffff", "Army Green": "#4b5320", "Ash Grey": "#b2beb5", "Asparagus": "#87a96b", "Banana Yellow": "#ffe135", "Beaver": "#9f8170", "Bitter Lime": "#bfff00", "Bittersweet Shimmer": "#bf4f51"}
-# print(len(name_to_code))
-
 colour_name = input("Colour name: ")
 while colour_name != "":
     if colour_name in NAME_TO_CODE:
