3a86f7b8314418aa7775cb26ab7226d6d9761227
dsantos-lean <lean.delossantos@my.jcu.edu.au>
2022-11-10 08:06:11 +1000

    Add error-checking functions, docstrings and comments

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,24 +2,46 @@ from prac_06.guitar import Guitar
 
 
 def main():
+    """Add and display guitars program, using Guitar class"""
     guitars = []
     print("My guitars!")
     name = input("Name: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: $ "))
-        guitar = Guitar(name, year, cost)
-        guitars.append(guitar)
-        print(f"{guitar} added.")
+    while name != "":  # Keep adding
+        year = get_valid_number("Year: ")
+        cost = float(get_valid_number("Cost: "))
+        add_guitars(cost, guitars, name, year)
         name = input("Name: ")
+    display_guitars(guitars)
 
-    """Test data"""
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
+def add_guitars(cost, guitars, name, year):
+    """Add guitar from user input to list of guitars"""
+    guitar = Guitar(name, year, cost)
+    guitars.append(guitar)
+    print(f"{guitar} added.")
+
+
+def get_valid_number(prompt):
+    """Get valid number with exception based error-checking"""
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            number = int(input(prompt))
+            if number <= 0:
+                print("Number must be >= 1")
+            else:
+                is_valid_input = True
+        except ValueError:
+            print("Invalid input; enter a valid number")
+    return number  # Works as intended
+
+
+def display_guitars(guitars):
+    """Display list of guitar names, years, cost and if they are vintage"""
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))  # Test data
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))  # Test data
     max_guitar_length = max([len(guitar.name) for guitar in guitars])
     for i, guitar in enumerate(guitars, 1):
-        # print(guitar)
         guitar.get_age()
         vintage_string = ""
         if guitar.is_vintage():
