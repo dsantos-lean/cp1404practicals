7cede33adc84903e4e88c0e91fbfe3f42a2fcdbe
dsantos-lean <lean.delossantos@my.jcu.edu.au>
2022-11-10 10:37:41 +1000

    Remove age and add comment

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,10 +1,10 @@
 class Guitar:
-    def __init__(self, name="", year=0, cost=0.0, age=0):
+    def __init__(self, name="", year=0, cost=0.0):
         self.name = name
         self.year = year
         self.cost = cost
-        self.age = age
 
+    # Comment out __str__ for Prac 7
     def __str__(self):
         return "{} ({}) : $ {}".format(self.name, self.year, self.cost)
 
